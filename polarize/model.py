4e09509134c44acedc719cf1b599b0c09f0a1012
Tom White <tom.e.white@gmail.com>
2024-09-08 20:18:47 +0100

    Polarize puzzle

---
+++ polarize/model.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from enum import Enum
+from itertools import product
+
+class PolarizingFilter(Enum):
+    def __new__(cls, value, char):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.char = char
+        return obj
+
+    POS_45 = (1, "/")
+    NEG_45 = (2, "\\")
+
+DominoOrientation = Enum("DominoOrientation", ["HORIZONTAL", "VERTICAL"])
+
+@dataclass
+class Domino:
+    filter1: PolarizingFilter
+    filter2: PolarizingFilter
+    orientation: DominoOrientation
+
+ALL_DOMINOS = [Domino(*p) for p in product(PolarizingFilter, PolarizingFilter, DominoOrientation)]
---
+++ polarize/tests/test_model.py
@@ -0,0 +1,6 @@
+
+from polarize.model import Domino, DominoOrientation, PolarizingFilter, ALL_DOMINOS
+
+def test_domino():
+    for domino in ALL_DOMINOS:
+        print(domino)
