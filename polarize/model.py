a9f4947fa635a04f29bb300c391a3bb31564ee3e
Tom White <tom.e.white@gmail.com>
2024-09-20 20:51:55 +0100

    Work on all_boards

--- polarize/model.py
+++ polarize/model.py
@@ -45,7 +45,9 @@ class PlacedDomino:
 class Board:
     def __init__(self, *placed_dominoes):
         self.values = np.zeros((4, 4), dtype=np.int8)
+        self.colours = np.zeros((4, 4), dtype=np.int8)
         self.n = 4  # assume board of size 4
+        self.n_dominoes = 0
         for placed_domino in placed_dominoes:
             self.add_domino(placed_domino)
 
@@ -54,11 +56,14 @@ class Board:
         domino = placed_domino.domino
         x, y = placed_domino.x, placed_domino.y
         self.values[y, x] = domino.filter1.value
+        self.colours[y, x] = self.n_dominoes + 1
         if domino.orientation == DominoOrientation.HORIZONTAL:
             x2, y2 = x + 1, y
         else:
             x2, y2 = x, y + 1
         self.values[y2, x2] = domino.filter2.value
+        self.colours[y2, x2] = self.n_dominoes + 1
+        self.n_dominoes += 1
 
     def can_add(self, placed_domino):
         x, y = placed_domino.x, placed_domino.y
@@ -76,4 +81,4 @@ class Board:
         return 0 < x <= self.n and 0 < y <= self.n
 
     def __str__(self):
-        return str(self.values)
+        return str(self.colours)
