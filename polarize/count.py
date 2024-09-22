c2ed21f29743f1e46e1914c262c838809c3cc8be
Tom White <tom.e.white@gmail.com>
2024-09-22 13:23:50 +0100

    Finding puzzles...

---
+++ polarize/count.py
@@ -0,0 +1,35 @@
+from itertools import permutations, product
+
+from polarize.model import Board, DominoOrientation, PlacedDomino
+
+def all_boards(dominoes):
+
+    def coord_lt(coord1, coord2):
+        y1, x1 = coord1
+        y2, x2 = coord2
+        return x1 + 4 * y1 < x2 + 4 * y2
+
+    def coords_increasing(coords):
+        for i in range(len(coords) - 1):
+            if not coord_lt(coords[i], coords[i + 1]):
+                return False
+        return True
+
+    for domino_perm in set(permutations(domino for domino in dominoes)):
+        for coords in product(*(domino.places() for domino in domino_perm)):
+            # skip if coords are not strictly increasing (in pos)
+            if not coords_increasing(coords):
+                continue
+
+            try:
+                board = Board()
+                for domino, coord in zip(domino_perm, coords):
+                    y, x = coord
+                    pd = PlacedDomino(domino, x, y)
+                    if board.can_add(pd):
+                        board.add_domino(pd)
+                    else:
+                        raise ValueError()
+                yield board
+            except ValueError:
+                pass  # try next coords
---
+++ polarize/tests/test_count.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from polarize.count import all_boards
+from polarize.model import ALL_DOMINOES
+
+def test_all_boards():
+    from rich.console import Console
+    console = Console()
+
+    dominoes = [ALL_DOMINOES[0], ALL_DOMINOES[3]]
+
+    for board in all_boards(dominoes):
+        # print(board)
+        console.print(board.to_rich())
+        print(board.lights())
+
+    boards = list(all_boards(dominoes))
+    lights = np.asarray([board.lights() for board in boards])
+
+    print("Total boards:", len(boards))
+    print(lights)
+
+    u, ui, uii, uc = np.unique_all(lights)
+    print(u)
+    print(ui)
+    print(uii)
+    print(uc)
+
+    # pick a non-unique solution at random and print the boards to see what we get
+    print(uc[-1], u[-1], np.nonzero(lights == u[-1])[0])
+
+    for ind in np.nonzero(lights == u[-1])[0]:
+        board = boards[ind]
+        console.print(board.to_rich())
+        print(f"{board.lights():08b}")
+
+    # find unique lights
+    print("unique puzzles")
+    print(u[uc == 1])
+    for li in u[uc == 1]:
+        inds = np.nonzero(lights == li)[0]
+        assert len(inds) == 1
+        ind = inds[0]
+        board = boards[ind]
+        console.print(board.to_rich())
+        print(f"{board.lights()}, {board.lights():08b}")
