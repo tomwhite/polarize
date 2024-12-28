f4c18ab2f71d5da590e311081f7a066a2d10fd51
Tom White <tom.e.white@gmail.com>
2024-12-28 12:39:12 +0000

    Improve generate

--- polarize/generate.py
+++ polarize/generate.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from polarize.count import all_boards
@@ -5,23 +7,31 @@ from polarize.model import ALL_DOMINOES
 
 def generate(n_pieces=None):
 
-    # choose some dominoes (TOOD: randomise)
-    dominoes = [ALL_DOMINOES[0], ALL_DOMINOES[3]]
+    while True:
+        # choose some dominoes
+        dominoes = random.choices(ALL_DOMINOES, k=3)
+        print(dominoes)
 
-    # find all the boards and lights for these dominoes
-    boards = list(all_boards(dominoes))
-    lights = np.asarray([board.lights for board in boards])
+        # find all the boards and lights for these dominoes
+        boards = list(all_boards(dominoes))
+        lights = np.asarray([board.lights for board in boards])
 
-    # find unique lights
-    u, ui, uii, uc = np.unique_all(lights)
+        # find unique lights
+        u, ui, uii, uc = np.unique_all(lights)
 
-    # choose a random puzzle
-    li = np.random.choice(u[uc == 1])
+        # check there are some unique puzzles
+        if np.count_nonzero(uc == 1) == 0:
+            continue
 
-    inds = np.nonzero(lights == li)[0]
-    assert len(inds) == 1
-    ind = inds[0]
-    board = boards[ind]
+        # choose a random puzzle
+        li = np.random.choice(u[uc == 1])
+
+        inds = np.nonzero(lights == li)[0]
+        assert len(inds) == 1
+        ind = inds[0]
+        board = boards[ind]
+
+        break
 
     from rich.console import Console
 
