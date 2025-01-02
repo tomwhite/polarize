1f12ddd22b875cddb27d0c814c5518aa5eddd80c
Tom White <tom.e.white@gmail.com>
2025-01-02 13:40:34 +0000

    Add solve function

---
+++ polarize/solve.py
@@ -0,0 +1,8 @@
+from polarize.count import all_boards
+
+def solve(puzzle):
+    solution_boards = []
+    for board in all_boards(puzzle.n, puzzle.dominoes):
+        if board.lights_int == puzzle.lights_int:
+            solution_boards.append(board)
+    return solution_boards
--- polarize/tests/test_generate.py
+++ polarize/tests/test_generate.py
@@ -1,6 +1,9 @@
 from polarize.generate import generate
-
+from polarize.solve import solve
 
 def test_generate():
     puzzle, solution = generate(4)
-    print(puzzle)
+    solutions = solve(puzzle)
+    assert len(solutions) == 1
+    assert solutions[0] == solution
+
